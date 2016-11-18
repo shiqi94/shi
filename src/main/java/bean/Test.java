862c41a7d88b7893805f7ed26864711b16753f78
shiqi94 <875931006@qq.com>
2016-11-18 17:21:20 +0800

    shiqi

--- src/main/java/bean/Test.java
+++ src/main/java/bean/Test.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class Test {
     public static void main(String[] args) {
-//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
-//        UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
-//        System.out.println(useFunctionService.SayHello("di"));
-//        context.close();
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
         UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
-        System.out.println(useFunctionService.SayHello("java"));
+        System.out.println(useFunctionService.SayHello("di"));
         context.close();
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+//        UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
+//        System.out.println(useFunctionService.SayHello("java"));
+//        context.close();
     }
 }
